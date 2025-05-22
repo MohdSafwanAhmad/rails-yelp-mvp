09b0e3d37349bdbf010f7196ac4a40697c4da9b9
33919848+MohdSafwanAhmad@users.noreply.github.com <safwanahmad006@gmail.com>
2025-05-21 22:32:15 -0400

    review not filling yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,7 +9,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :restaurants do
-    resources :reviews, only: [:index, :new, :create]
+    resources :reviews, only: [:new, :create]
   end
-  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
